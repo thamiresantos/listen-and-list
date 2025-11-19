0aa1020941967fe0dd0dacd148b62e15e09fd28a
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2025-11-18 23:54:23 -0300

    feat: Exibe playlist e info do usuário na tela

--- javascript/playlistFinal.js
+++ javascript/playlistFinal.js
@@ -7,3 +7,24 @@ const titulo = document.getElementById("titulo");
 const subtitulo = document.getElementById("sub-titulo");
 const listaFinal = document.getElementById("playlist-final");
 
+// MOSTRAR O NOME DO USUÁRIO NO TÍTULO
+if (nomeUsuario) {
+    titulo.textContent = `Playlist de ${nomeUsuario} Finalizada!`;
+}
+
+//  MOSTRAR QUANTAS MÚSICAS ELE ADICIONOU
+subtitulo.textContent = `Você selecionou ${playlist.length} músicas incríveis para a sua coleção pessoal!`;
+
+// MOSTRAR PLAYLIST NA TELA
+playlist.forEach(musica => {
+    const item = document.createElement("div");
+    item.classList.add("item-final");
+
+    item.innerHTML = `
+        <p><strong>${musica.titulo}</strong></p>
+        <span> ${musica.artista} • ${musica.tempo} </span>
+    `;
+
+    listaFinal.appendChild(item);
+});
+
